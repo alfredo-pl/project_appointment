c90dcf23c393bd36bd87c2a4742253184927b50d
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-28 12:06:42 -0400

    change route devise controller and modify inputs registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,7 +48,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys:  [:name, :run, :direction, :phone])
-
+  end
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'landing/index'
   resources :appointment_apps
   resources :atention_schedules
   resources :branchoffices
   resources :businesses
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
+  devise_for :users , controllers: {
+    registrations: 'users/registrations'
   }
   root to: "landing#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
