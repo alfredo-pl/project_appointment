0a351d906dffee90606c564986c139b377aae67d
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-28 12:07:37 -0400

    gem active admin and config

---
+++ app/admin/businesses.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Business do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :email, :rut
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :email, :rut]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
