6d84b45959b34963320f3114bb3f6a2b9506d765
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-28 11:12:15 -0400

    controller landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'landing/index'
   resources :appointment_apps
   resources :atention_schedules
   resources :branchoffices
@@ -6,6 +7,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root to: "home#index"
+  root to: "landing#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
