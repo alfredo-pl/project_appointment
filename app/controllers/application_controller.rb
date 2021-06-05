4b701ba6a05e440617129837efe616301d9c3b09
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-05 11:11:20 -0400

    after sing in path for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-    def after_sign_in_path_for(resource)
-        stored_location_for(resource) || dashboard_user_path
+    def after_sign_in_path_for(current_user)
+        if current_user.role = 1
+            businesses_path
+        else
+            dashboard_user_path
+        end
     end
 end
