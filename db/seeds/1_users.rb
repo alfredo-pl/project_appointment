9dd8c8655f27392d91bc7ad1fa68c282aebac499
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-05 11:03:47 -0400

    add role user 1

--- db/seeds/1_users.rb
+++ db/seeds/1_users.rb
@@ -1,6 +1,6 @@
 User.destroy_all
 puts "Creating Users"
-User.create!(email: "alejandro@example.cl", name: "Alejandro Pietro", password: "123123", password_confirmation:"123123")
+User.create!(email: "alejandro@example.cl", name: "Alejandro Pietro", password: "123123", password_confirmation:"123123", role:1)
 User.create!(email:"marcela@example.cl", name: "Marcela Valenzuela", password: "123123", password_confirmation:"123123")
 User.create!(email:"alba@example.cl", name: "Alba Mendez" , password: "123123", password_confirmation:"123123")
 
