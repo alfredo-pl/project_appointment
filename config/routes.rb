2e3b2f3ba6f6d30e7f6c40f019eb76a5d9c9fae0
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-06 10:22:49 -0400

    dashboard business

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'business/:id', to: 'businesses#show_user'
+  get 'dashboard_business', to: 'businesses#dashboard_business'
   devise_for :users , controllers: {
     registrations: 'users/registrations'
   }
