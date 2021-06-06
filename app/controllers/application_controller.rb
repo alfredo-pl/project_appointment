aaee21bb9844035a76484cc05c76383480ca671e
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-06 11:24:03 -0400

    dashboard business to user role 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(current_user)
         if current_user.role == 1
-            businesses_path
+            dashboard_business_path
         else
             dashboard_user_path
         end
