8320f6df2478af686bd1c032df707bdf5a5bfb1a
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-06 10:37:21 -0400

    fixed if

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(current_user)
-        if current_user.role = 1
+        if current_user.role == 1
             businesses_path
         else
             dashboard_user_path
