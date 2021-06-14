43342c791e3ac726e4b3d66d97140fd0c2554d7a
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-14 10:00:07 -0400

    method to get count appoinment for business

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,12 @@
 class Business < ApplicationRecord
     has_many :branchoffices , dependent: :destroy
     belongs_to :user
+
+
+    #method to get count appoinment for business
+    def self.get_count_appointment(id)
+        branch = Branchoffice.references(:business).where(business_id: id)
+        appointments = branch.map{|b| AppointmentApp.references(:branchoffice).where(branchoffice_id: b.id).count }
+        appointments.sum
+    end
 end
